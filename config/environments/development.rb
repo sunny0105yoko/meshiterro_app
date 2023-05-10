5ba8d397e1fa7c4949fa8c5292128e7d067e9961
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-05-10 00:31:37 +0000

    [start]5-8

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "79e17981452b442e941eec2cfc500338.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
